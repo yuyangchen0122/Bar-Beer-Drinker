c10f99ebc5f8425d80a74b94fb0f7933cb4d1dac
yuyangchen0122 <chenyuyang0122@gmail.com>
2018-10-27 21:49:41 -0400

    add bills transasaction

--- transactiongenerate.py
+++ transactiongenerate.py
@@ -4,9 +4,10 @@ import random
 import numpy as np
 
 
-def lookup_trans_bar(bill_transaction_id_list, bill_license_list, transaction_id):
+def lookup_trans_bar(bill_transaction_id_list, bill_license_list, transaction_id, sell_name_list):
     index = bill_transaction_id_list.index(transaction_id)
     return bill_license_list[index]
+    return sell_name_list[index]
 
 
 def lookup_item_price(index_of_bar_license, sell_item_code_list, sell_price_list):
@@ -22,16 +23,16 @@ def random_item(list, bar_license):
     return index_of_bar_license
 
 
-sells = pd.read_csv("Sell.csv")
+sells = pd.read_csv("Sells.csv")
 bill = pd.read_csv("BILL.csv")
 
-sell_name_list = sells.Name.tolist()
-sell_license_list = sells.bar_lisence.tolist()
-sell_item_code_list = sells.item_code.tolist()
-sell_price_list = sells.price.tolist()
+sell_license_list = sells.License.tolist()
+sell_item_code_list = sells.ItemID.tolist()
+sell_price_list = sells.Price.tolist()
 
 bill_license_list = bill.License.tolist()
-bill_transaction_id_list = bill.transaction_id.tolist()
+bill_transaction_id_list = bill.TransactionID.tolist()
+bill_name_list = bill.Bar.tolist()
 
 
 file = open('transaction.csv', 'w')
@@ -39,11 +40,12 @@ line_count = 0
 
 
 for i in range(len(bill_transaction_id_list)):
-    t = random.randint(1, 8)
+    t = random.randint(1, 12)
 
     for j in range(t):
         transaction_id = bill_transaction_id_list[i]
         bar_license = bill_license_list[i]
+        bar_name = bill_name_list[i]
         ind = random_item(sell_license_list,bar_license)
         item = lookup_item_price(ind, sell_item_code_list, sell_price_list)
         s = ','
